c931aa83866fa7fbfb8eaceb9dbb980be4f923a3
Tara Hulme <tgosser137@gmail.com>
2020-01-04 16:28:44 -0500

    updated radio buttons on wireframe

--- main.js
+++ main.js
@@ -2,12 +2,65 @@
 var name = "tara";
 
 //function when a user clicks on the start button
+function startQuiz() {
+    $('#start').on('click', function(event) {
+        console.log("render the question element");
+        renderAQuestion();    
+    }
+    );
 
+}
 //shows the question number and score
+function updateQuestionAndScore() {
+    console.log("show question number and score")
+    const html = $(`<ul>
+        <li id="js-answered">Question Number: 
+        ${STORE.currentQuestion + 1}/
+        ${STORE.questions.length}</li>
+        <li id="js-score">Score: 
+        ${STORE.score}/
+        ${STORE.questions.length}</li>
+        </ul>`);
+    $(".question-and-score").html(html);
+}
 
 //display the options for the current question
 
-//display the questions
+//display the questions -renderAQuestion()
+function renderAQuestion() {
+    console.log("showing the current question");
+    let question = STORE.questions
+    [STORE.currentQuestion];
+    updateQuestionAndScore();
+    const questionHtml = $(`
+    <div>
+        <form id="js-questions" class="question-form">
+        <fieldset>
+            <div class="row question">
+                <div class="col-12">
+                    <legend> ${question.question} </legend>
+                </div>
+            </div>
+
+            <div class="row options">
+                <div class="col-12">
+                <div class="js-options"> </div>
+                </div>
+            </div>
+    
+            <div class="row">
+                <div class="col-12">
+                    <button type = "submit" id="answer" tabindex="5">Submit</button>
+                    <button type = "button" id="next-question" tabindex="6"> Next >></button>
+                </div>
+            </div>
+        </fieldset>
+        </form>
+    </div>`);
+$("main").html(questionHtml);
+updateOptions();
+$("#next-question").hide();
+}
 
 //display results and restart quiz button
 
